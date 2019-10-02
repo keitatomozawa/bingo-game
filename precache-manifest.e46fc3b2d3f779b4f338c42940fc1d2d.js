fa75fca07abd837349955e81c81d78e0d50a0a75
keitm <keita.tmzw@gmail.com>
2019-10-02 13:50:49 +0900

    Updates

---
+++ precache-manifest.e46fc3b2d3f779b4f338c42940fc1d2d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aeb28fd52976465103e37a32307ca49",
+    "url": "/bingo-game/index.html"
+  },
+  {
+    "revision": "8c23fe51126d162b5ff9",
+    "url": "/bingo-game/static/js/2.e833e581.chunk.js"
+  },
+  {
+    "revision": "0c28c2f1daa2c5a24496",
+    "url": "/bingo-game/static/js/main.50a62d16.chunk.js"
+  },
+  {
+    "revision": "b2839ed4952eddd9b966",
+    "url": "/bingo-game/static/js/runtime-main.79deb03b.js"
+  }
+]);
